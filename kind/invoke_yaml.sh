19388853355607cceff451080181cdb2562a23b6
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-03-15 12:21:23 +0530

    initial commit

---
+++ kind/invoke_yaml.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
+echo ""
+echo "*******************************"
+echo "  Kubernetes in Docker (KIND)  "
+echo "*******************************"
+echo ""
+echo "This script creates a KIND cluster along with worker nodes to test Kubernetes features"
+echo "Script begins now..."
+echo ""
+kind create cluster --config k8_setup.yaml
+echo "checking the cluster and node creation part.."
+kubectl get nodes 
+sleep 5
+kubectl get nodes
