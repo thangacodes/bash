7fadc9a082ff0fb68038d1cc064a70792eb4d9cb
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-03-15 18:59:23 +0530

    updates

--- kind/kind_create_delete_cluster.sh
+++ kind/kind_create_delete_cluster.sh
@@ -2,7 +2,7 @@
 echo "Script executed at: $(date '+%Y-%m-%d %H:%M:%S')"
 echo ""
 echo "*******************************"
-echo "  Kubernetes in Docker (KIND)  "
+echo "  Kubernetes IN Docker (KIND)  "
 echo "*******************************"
 echo ""
 echo "This script creates a KIND cluster along with worker nodes to test Kubernetes features"
