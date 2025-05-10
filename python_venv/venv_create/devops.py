d8876e8e844eb54862eae33cb04cde787a2b24a3
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-05-10 14:34:06 +0530

    Update devops.py

--- python_venv/venv_create/devops.py
+++ python_venv/venv_create/devops.py
@@ -14,25 +14,7 @@ def monitoring_skills():
 def scripting_skills():
     script_names = ["bash", "java-script", "python", "Shell"]
     return random.choice(script_names)
-
-# Call the function and print the result
-import random
-def cloud_skills():
-    cloud = ["AWS", "Azure", "GCP", "OCI", "VMware"]
-    return random.choice(cloud)
-def cm_tools():
-    tool_names=["git","githubAction","jenkins","ansible","terraform","vault"]
-    return random.choice(tool_names)
-def container_skills():
-    cont_names = ["docker", "docker-compose", "ECS", "EKS", "Kubernetes"]
-    return random.choice(cont_names)
-def monitoring_skills():
-    monit_names = ["datadog", "prometheus", "grafana", "dynatrace", "AppDynamics"]
-    return random.choice(monit_names)
-def scripting_skills():
-    script_names = ["bash", "java-script", "python", "Shell"]
-    return random.choice(script_names)
-
+    
 # Call the function and print the result
 if __name__ == "__main__":
     print(f"You should have knowledge of at least one public cloud provider: {cloud_skills()}")
